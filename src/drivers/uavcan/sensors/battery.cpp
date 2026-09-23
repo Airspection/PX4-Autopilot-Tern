d7ad9456e47a72a615fe97bcb0ec412734f2c6fa
Konstantinos Gkogkalatze <konstantinos.gkogkalatze@airspection.co.uk>
2026-09-23 13:29:50 +0100

    style: Fix formatting changes

--- src/drivers/uavcan/sensors/battery.cpp
+++ src/drivers/uavcan/sensors/battery.cpp
@@ -84,12 +84,10 @@ int UavcanBatteryBridge::init()
 void
 UavcanBatteryBridge::battery_sub_cb(const uavcan::ReceivedDataStructure<uavcan::equipment::power::BatteryInfo> &msg)
 {
-	
 	// Ignore unwanted DroneCAN battery sensor node ID 51
 	if (msg.getSrcNodeID().get() == 51) {
 		return;
 	}
-
 
 	uint8_t instance = 0;
 
